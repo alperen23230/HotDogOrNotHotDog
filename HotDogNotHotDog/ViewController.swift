88696e02f20972dbd07c88a375e85819442ebdbf
alperen23230 <alperenunal68@gmail.com>
2019-08-14 13:47:42 +0300

    Initial Commit

---
+++ HotDogNotHotDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDogNotHotDog
+//
+//  Created by Alperen Ünal on 14.08.2019.
+//  Copyright © 2019 Alperen Ünal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
